83e3470cca49075aafe2b6de426e01b73e3cf724
jawaadmerali <113218507+jawaadmerali@users.noreply.github.com>
2023-08-11 17:40:05 -0500

    Create mortgagetools.vue

---
+++ mortgagetools.vue
@@ -0,0 +1,25 @@
+<template>
+    <section class="inline large">
+        <div class="grid fixed fixed-max-sm large grid-xs-1">
+            <div class="text-center">
+                <h2><b class="f3 text-bold">{{title}}</b> </h2>
+            </div>
+            <div class="list-card">
+                <mvtLazyload ref="lazyload" data-role="mortgagetoolcard">
+                    <mvtShifter ref="shifter" :center="true" v-if="mortgageData && mortgageData.length">
+                        <infoCard v-for="item in mortgageData"
+                            :key="item.id"
+                            :headerText="item.title"
+                            :descriptionText="item.description"
+                            :buttonLabel="item.action"
+                            :buttonHref="glb.appUrl + item.href"
+                            textAlign="left"
+                        />
+                    </mvtShifter>
+                </mvtLazyload>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script src="./mortgagetools.js"></script>
